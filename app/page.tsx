6f91f817eef847ab9eba94d37774b725380a5a78
Randy Mireku <rhandymireku200@gmail.com>
2025-11-13 16:39:08 +0000

    Update 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import React from "react";
+import { FaMapMarkedAlt, FaSearch } from "react-icons/fa";
 
 const VideoBackground = () => {
   return (
@@ -18,14 +18,35 @@ const VideoBackground = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-black/30"></div>
 
       {/* Content */}
-      <div className="relative z-10 flex items-center justify-center h-full text-center text-white px-4">
-        <div>
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            Welcome to My Site
-          </h1>
-          <p className="text-lg md:text-2xl">
-            Hello! This is a video background with Tailwind CSS in Next.js.
-          </p>
+     <div className="relative z-10 min-h-screen flex items-center justify-center">
+        {/*Card Container*/}
+        <div className="bg-white shadow-lg mt-10 p-4 rounded w-full max-w-sm mx-4">
+           <div className="flex">
+            {/*Input Field & search button*/}
+            <div className="flex border rounded items-center px-2 py-2 w-full">
+            <FaSearch className="h-5 w-5"/>
+            <input
+              type="text"
+              placeholder="Enter City Name"
+              className="pl-2 border-none focus:outline-none"
+            />
+          </div>
+
+          {/* current location button */}
+          <button className="px-4 py-2 bg-blue-300 text-white ml-2 rounded hover:bg-blue-600">
+            <FaMapMarkedAlt className="w-5 h-5"/>
+          </button>
+
+        </div>
+       
+      {/* Weather Data Display */}
+      <div className="mt-4 text-center">
+        <h2 className="text-xl font-semibold">Madrid</h2>
+        {/* Weather Icon */}
+        <p className="text-lg font-semibold">2 °C</p>
+        <p className="text-sm capitalize font-semibold"> Cloudy </p>
+       </div>
+
         </div>
       </div>
     </div>
