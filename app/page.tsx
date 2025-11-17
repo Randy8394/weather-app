f0407745afb1706eef54110588a2fec98a03fc8c
Randy Mireku <rhandymireku200@gmail.com>
2025-11-17 15:54:47 +0000

    API First Integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+'use client';
+
+import type React from "react";
 import { FaMapMarkedAlt, FaSearch } from "react-icons/fa";
 import HourlyForecast from "@/public/components/HourlyForecast";
 import img from "../public/media/weather.png";
+import axios from "axios";
+import {useState, useEffect} from "react";
 
 const VideoBackground = () => {
+
+   const [weatherData, setWeatherData] = useState<any>(null);
+   const [city, setCity] = useState('MADRID');
+
+  const api_key = "958405fb95e440b7bdb132400251711";
+  const api_url = "http://api.weatherapi.com/v1/forecast.json";
+
+  const fetchData = async () => {
+    try {
+      const url = `${api_url}?key=${api_key}&q=${city}&days=1`;
+      console.log('Fetching weather data from URL:', url);
+      const response = await axios.get(url);
+      console.log('API Response:', response.data);
+      setWeatherData(response.data);
+      }
+
+      catch(err){
+        console.error("There was an error or the city was not found:", err);
+      }
+    };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      fetchData();
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log('Rendering with weatherData:', weatherData);
+
   return (
     <div className="relative w-full h-screen overflow-hidden">
       {/* Video Background */}
@@ -30,6 +67,9 @@ const VideoBackground = () => {
             <input
               type="text"
               placeholder="Enter City Name"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              onKeyUp={handleKeyPress}
               className="pl-2 border-none focus:outline-none"
             />
           </div>
@@ -42,13 +82,15 @@ const VideoBackground = () => {
         </div>
        
       {/* Weather Data Display */}
-      <div className="mt-4 text-center">
-        <h2 className="text-xl font-semibold">Madrid</h2>
-        {/* Weather Icon */}
-        <img src={img.src} alt="Weather icon" className="mx-auto h-40"/>
-        <p className="text-lg font-semibold">2 °C</p>
-        <p className="text-sm capitalize font-semibold"> Cloudy </p>
-       </div>
+      {weatherData?.location && (
+        <div className="mt-4 text-center">
+          <h2 className="text-xl font-semibold">{weatherData.location.name}</h2>
+          {/* Weather Icon */}
+          <img src={weatherData.current.condition.icon} alt="Weather icon" className="mx-auto h-40"/>
+          <p className="text-lg font-semibold">{weatherData.current.temp_c}°C</p>
+          <p className="text-sm capitalize font-semibold">{weatherData.current.condition.text}</p>
+        </div>
+      )}
 
        {/*Hourly Forecast*/}
        <HourlyForecast/>
